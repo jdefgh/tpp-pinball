559b77271f3c2637b4d8b016ce5f84450a648ba4
jdefgh <67150895+jdefgh@users.noreply.github.com>
2021-04-10 20:01:45 +0200

    Add the code

---
+++ pinballData.py
@@ -0,0 +1,35 @@
+import re
+import socket
+import time
+
+PINBALL = re.compile("([\d,]*[\d]+) points \(x(\d+)\) on the (red|blue|gold|silver) pinball table",)
+
+TABLES = ["red", "blue", "gold", "silver"]
+
+failed_auth_attempts = 0
+
+def connect():
+    s.connect(("irc.chat.twitch.tv", 6667))
+    s.send(b"NICK justinfan123\n")
+    s.send(b"JOIN #twitchplayspokemon\n")
+s = socket.socket()
+connect()
+
+while 1:
+    try:
+        data = s.recv(2048).decode("utf-8").split("\r\n")[:-1]
+    except ConnectionResetError:
+        connect()
+        time.sleep((failed_auth_attempts - 1) ** 2 * bool(failed_auth_attempts))
+        continue
+    for i in data:
+        if PINBALL.search(i) is not None and i.startswith(":tpp"):
+            x = PINBALL.split(i)
+            print(x)
+            with open("pinball.dat", "ab") as file:
+                table = TABLES.index(x[3])
+                score = int(x[1].replace(",", "")) // 50
+                data = ((score << 2) + table)
+                data = data.to_bytes(4, "big")
+                file.write(data)
+                file.close()
